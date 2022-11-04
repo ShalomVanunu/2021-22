b625cc8535a2d3838de2460f09422cef0db48ca2
shalomvanunu <shalomvanunu@gmail.com>
2022-11-04 22:39:16 +0200

    Updated 4/11

--- Flask/Bootstrap.py
+++ Flask/Bootstrap.py
@@ -5,7 +5,7 @@ app = Flask(__name__) # open object of flask
 
 @app.route("/")
 def sport():
-    return render_template("Input.html")
+    return render_template("Bootstrap.html")
 
 if __name__ == "__main__":
     app.run(port =80, host="0.0.0.0", debug=True)
