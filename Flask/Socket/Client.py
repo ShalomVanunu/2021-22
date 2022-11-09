28b0a3e0ac024f7b4485e2a78ec1337361de9294
ShalomVanunu <shalomvanunu@gmail.com>
2022-11-09 19:06:58 +0200

    Updated 9/11

---
+++ Flask/Socket/Client.py
@@ -0,0 +1,27 @@
+import socket
+
+
+
+def socket_init():
+    global client
+    PORT = 4444
+    IP = "172.20.129.27"
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP,PORT))
+    return client
+
+def send_data(data):
+    print(data)
+    print(client)
+    client.send(data.encode())
+
+def recv():
+    return client.recv(1024).decode()
+
+if __name__ == "__main__":
+    socket_init()
+    while True:
+        msg = input(" Enter Message :")
+        send_data(msg)
+        print(recv())
---
+++ Flask/Socket/Server.py
@@ -0,0 +1,19 @@
+import socket
+
+PORT = 4444 #above 1024
+IP = "172.20.129.27"
+
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # DGARM - UDP || STREAM -TCP
+server.bind((IP,PORT))
+server.listen()
+print(" waiting for clients")
+conn,addr = server.accept()
+print(addr)
+
+while True:
+    msg = conn.recv(1024).decode()
+    print(msg)
+    data = input(" Write msg to send :")
+    conn.send(data.encode())
+
